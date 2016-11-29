11bacc21b7f90be9f4dd6b002544c1e5249cd6cd
jesperbay <jesper.bay@live.dk>
2016-11-29 09:19:41 +0100

    Add project files.

---
+++ SimpleBindingMVVM/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBindingMVVM
+{
+    class Model
+    {
+        public string Name { get; set; }
+
+        public Model(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}", Name);
+        }
+    }
+}
---
+++ SimpleBindingMVVM/ViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleBindingMVVM.Annotations;
+
+namespace SimpleBindingMVVM
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+
+        private ObservableCollection<Model> _models;
+
+        public ObservableCollection<Model> Models
+        {
+            get { return _models; }
+            set { _models = value; }
+        }
+
+        public ViewModel()
+        {
+            _models = new ObservableCollection<Model>();
+            _models.Add(new Model("Henrik"));
+            _models.Add(new Model("Peter"));
+            _models.Add(new Model("Vibeke"));
+            Name = "Navn";
+
+        }
+
+        public void Add() { Models.Add(new Model(Name)); OnPropertyChanged();}
+
+
+        #region PropertyChangeSupport
+       public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        
+    }
+}
